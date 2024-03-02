74a2d7bfb5693ed98b9f2bc04a929af0eadf34e1
shajib-khan <shajibkhan946@gmail.com>
2024-03-02 10:12:14 +0600

    start the project

---
+++ database/migrations/2024_02_29_115702_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+use App\Models\Job;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedInteger('salary');
+            $table->string('location');
+            $table->string('category');
+            $table->enum('experience', Job::$experience);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
