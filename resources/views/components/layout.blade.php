06950806e14a3d5fb63ddd9c8be2026111a63836
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-03-04 17:27:33 +0600

    fixed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel Job Board</title>
-        @vite('resources/css/app.css','resources/js/app.js')
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
 
   <body class="from-10% via-30% to-90% mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
